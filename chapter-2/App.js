805150143095294c13479e694db103f0dbf18ef6
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-10-17 19:25:26 +0530

    igniting -app

---
+++ chapter-2/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+
+const heading = React.createElement("h1",{},"Heading");
+const heading1 = React.createElement("h1",{},"Heading1");
+const Container = React.createElement("div",{id:"container"},[heading,heading1])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Container);
---
+++ chapter-2/dist/index.6bd02f5a.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("h1", {}, "Heading");
+const heading1 = React.createElement("h1", {}, "Heading1");
+const Container = React.createElement("div", {
+    id: "container"
+}, [
+    heading,
+    heading1
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Container);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
